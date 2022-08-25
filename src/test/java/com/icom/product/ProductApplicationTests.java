c012dca11373512583915799a87a7c376ac06418
Your Name <you@example.com>
2022-08-25 14:21:05 +0700

    ++ unit test for managed-database in container

--- src/test/java/com/icom/product/ProductApplicationTests.java
+++ src/test/java/com/icom/product/ProductApplicationTests.java
@@ -1,13 +1,36 @@
 package com.icom.product;
 
+import com.icom.product.entity.ProductCategoryEntity;
+import com.icom.product.repository.ProductCategoryEntityRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ProductApplication.class)
+@ActiveProfiles("test")
 class ProductApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ProductCategoryEntityRepository productCategoryEntityRepository;
+
+    @Test
+    public void test_insert_and_retrieve_data_for_unittest_postgres_container() {
+        ProductCategoryEntity productCategory = new ProductCategoryEntity();
+        productCategory.setProductCategoryName("test product name");
+        productCategoryEntityRepository.save(productCategory);
+
+        Iterable<ProductCategoryEntity> all = productCategoryEntityRepository.findAll();
+        int counter = 0;
+        for (Object i : all) {
+            counter++;
+        }
+        assertThat(counter).isEqualTo(1);
+    }
 
 }
