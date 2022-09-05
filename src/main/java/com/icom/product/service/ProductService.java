f8db75e9c6d57a445b8e0cfd02b9a5be7c748ac5
Your Name <you@example.com>
2022-09-05 16:36:20 +0700

    enable actuator for beans

--- src/main/java/com/icom/product/service/ProductService.java
+++ src/main/java/com/icom/product/service/ProductService.java
@@ -17,8 +17,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
